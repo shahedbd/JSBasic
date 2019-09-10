1a7d00983bcbeb66a576e983d7d75dcd9489ae95
shahedbd <shahed.mbstu@gmail.com>
2019-09-10 22:23:55 +0600

    + WIP

---
+++ IndexedDB/basic.js
@@ -0,0 +1,12 @@
+// https://www.tutorialspoint.com/html5/html5_indexeddb.htm
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || 
+window.msIndexedDB;
+ 
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || 
+window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || 
+window.webkitIDBKeyRange || window.msIDBKeyRange
+ 
+if (!window.indexedDB) {
+   window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
---
+++ LStorage/basic.js
@@ -0,0 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
+
+localStorage.setItem('name','Chris');
+var myName = localStorage.getItem('name');
+
+console.log(myName)
+
